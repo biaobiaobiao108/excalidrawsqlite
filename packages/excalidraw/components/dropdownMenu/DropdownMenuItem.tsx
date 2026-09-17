56c5c6babaf0c87d449ade5fb6ab17c0ed62ef90
biaobiaobiao108 <steelway108@gmail.com>
2026-09-17 15:58:50 +0800

    fix: update Cascadia CDN family references

--- packages/excalidraw/components/dropdownMenu/DropdownMenuItem.tsx
+++ packages/excalidraw/components/dropdownMenu/DropdownMenuItem.tsx
@@ -81,7 +81,7 @@ export const DropDownMenuItemBadge = ({
     padding: "2px 4px",
     borderRadius: 6,
     fontSize: 9,
-    fontFamily: "Cascadia, monospace",
+    fontFamily: "Cascadia Code, monospace",
     border: theme === THEME.LIGHT ? "1.5px solid white" : "none",
   };
 
